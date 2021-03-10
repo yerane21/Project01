d334a8aed429a99da71fd81790410b09c1b592d6
yerane21 <yerane21@gmail.comgit congig --global user.email :yerane21@gmail.com>
2021-03-10 10:57:21 +0530

    modified files of the method

--- Demo01.java
+++ Demo01.java
@@ -1,2 +1,5 @@
 public class Demo01  {
+     static void m1() {
+       System.out.println("Welcome to java");
+   }
 }
--- Demo02.java
+++ Demo02.java
@@ -1,5 +1,6 @@
 public class Demo02 {
   public static void main(String[] args){
     System.out.println("Welcome to github");
+    System.out.println("Welcome to shubham");
   }
 }
